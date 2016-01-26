8324be3abb8b75edbff089c66a7418f1ed11cc8a
Valinthronix <jmeyerlee@gmail.com>
2016-01-26 11:08:28 -0500

    Solved Part 4
    Added a pprint of the game history in the form of a 2 column table (might be two rows I'm not sure, also might not work at all)
    Added the actual die roll function

--- SnakesLadders/SnakesLadders.py
+++ SnakesLadders/SnakesLadders.py
@@ -1,3 +1,6 @@
+from random import randint
+import pprint
+
 class State(object):
     def __init__(self, ix):
         self.index = ix
@@ -16,6 +19,8 @@ class State(object):
             # link is None: "Normal" = not a snake or ladder
             return self.index
 
+def roll(): return randint(1,6)
+
 class GameFSM(object):
     def __init__(self, n):
         self.all_states = []
@@ -23,14 +28,19 @@ class GameFSM(object):
         self.n = n 
         for ix in range(n+1):
             self.all_states.append(State(ix))
+        self.history = (None, None)
+        for i in self.history:
+            i = []
             
     def move(self, die):
         """die is an integer
         """
+        self.history[0].append(die)
         inter_pos = self.position + die
         state_obj = self.all_states[inter_pos]
         final_pos = state_obj.process()
         self.position = final_pos
+        self.history[1].append(self.position)
         
     def run(self):
         print("Starting game!")
@@ -41,6 +51,9 @@ class GameFSM(object):
             self.move(die)
             # record results
         print("Game over!")
+        
+    def story(self):
+        pprint.pprint(self.history)
         
 game = GameFSM(16)
 
